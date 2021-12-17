9c779eaa2247acf3bd640a68154185aba0b78ab3
Ali <74180145+Ali-QS@users.noreply.github.com>
2021-12-17 11:47:27 +0200

    updated "MobileSendTransaction" script

--- Web3Unity/Scripts/Prefabs/Mobile/MobileSendTransaction.cs
+++ Web3Unity/Scripts/Prefabs/Mobile/MobileSendTransaction.cs
@@ -6,6 +6,14 @@ using UnityEngine.UI;
 
 public class MobileSendTransaction : MonoBehaviour
 {
+    public Text ReceiptText;
+    public Text AccountText;
+
+    private void Start()
+    {
+        AccountText.text = "Account: " + PlayerPrefs.GetString("Account");
+    }
+
     async public void OnMobileSendTransaction() {
         string to = "0xdA064B1Cef52e19caFF22ae2Cc1A4e8873B8bAB0";
         string value = "12300000000000000";
@@ -13,5 +21,6 @@ public class MobileSendTransaction : MonoBehaviour
         string data = "0x";
         string receipt = await Web3Mobile.SendTransaction(to, value, gas, data);
         print(receipt);
+        ReceiptText.text = "Receipt: " + receipt;
     }
 }
